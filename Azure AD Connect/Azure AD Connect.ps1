1f8ee4b153b6bc3d88ed29da303cd913d8c956c0
RoboticScript <128586683+RoboticScript@users.noreply.github.com>
2024-03-18 16:46:18 +0530

    Update Azure AD Connect.ps1

--- Azure AD Connect/Azure AD Connect.ps1
+++ Azure AD Connect/Azure AD Connect.ps1
@@ -1,1 +1,6 @@
+# https://learn.microsoft.com/en-us/entra/identity/hybrid/connect/how-to-connect-sync-feature-scheduler
+# https://activedirectorypro.com/force-sync-azure-ad-connect-using-powershell/
 
+Get-ADSyncScheduler
+Start-ADSyncSyncCycle -PolicyType Delta
+Start-ADSyncSyncCycle -PolicyType Initial
